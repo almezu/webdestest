d2b8220b6717a0a7d018b4c023baed345e7da540
almezu <74825011+almezu@users.noreply.github.com>
2020-11-22 22:54:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" href="css/stylePHP.css" type="text/css">
+ <meta charset="UTF-8">
+ </head> 
+<body> 
+	<h1>Responsive Image Grid Example</h1>
+		<div class=“outer-grid”>
+			<div class=“inner-grid”>
+				<a href=“hoami.html”>
+					<img src=“https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+					</a>
+					<img src=“https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+					<img src=“https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+			</div>
+			<div class=“inner-grid”>
+				<img src=“https://images.pexels.com/photos/38105102pexels-photo-38105102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+				<img src=“https://images.pexels.com/photos/38105102/pexels-photo-38105102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+				<img src=“https://images.pexels.com/photos/38105102/pexels-photo-38105102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+			</div>
+			<div class=“inner-grid”>
+				<img src=“https://images.pexels.com/photos/3863778/pexels-photo-386778.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+				<img src=“https://images.pexels.com/photos/386778/pexels-photo-386778.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+				<img src=“https://images.pexels.com/photos/386778/pexels-photo-386778.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500”/>
+			</div>
+		</div>
+</body>
+</html>
